8d45e0b3179d6d6a2a7f4341b0e4aa54eb1b999c
lokesh malviya <lokeshmalviya19112002@gmail.com>
2021-12-03 00:24:47 +0530

    lokesh

---
+++ Script.js
@@ -0,0 +1,10 @@
+<div class="box">Timeline</div>
+<div class="timelines"></div>
+    <div class="contain" style="position: absolute;top: 60%;left : 7%;"><p>September 18 2021</p>
+            <div class="containerleft" ><br>
+            <img src="D:\Eventpage.github.io\Gsoc.jpeg" class="image1" align="left"><BR CLEAR=”left” />
+            A 90 minutes fun-packed coding contest helping you all leverage your coding skills.🔥Along with sweets and barfis let's try to share knowledge, create an impact by helping one grow. 💫
+            Fill in the form and get ready to get your Prashad from Ganpati Bappa. 🥳
+            <div class="red">Continue</div>
+            </div>
+        </div>
